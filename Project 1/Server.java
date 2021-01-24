912d8e7fbd822d7e04507c62b57f3a1256d26a2d
cqh99 <cqh99@uw.edu>
2021-01-23 20:03:23 -0800

    server stage A & B sketch

--- Project 1/Server.java
+++ Project 1/Server.java
@@ -4,8 +4,16 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class Server {
-    public static final String HOSTNAME = "attu2.cs.washington.edu";
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        //??
+        ServerThread server1 = new ServerThread("A");
+        server1.start();
+        Thread.sleep(1000); // wait one sec before new thread
+
+        ServerThread server2 = new ServerThread("B");
+        server2.start();
+        Thread.sleep(1000);
+
 
     }
 }
--- Project 1/ServerThread.java
+++ Project 1/ServerThread.java
@@ -1,5 +1,10 @@
+import javax.xml.crypto.Data;
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * This thread is responsible to handle client connection for multi-thread usage.
@@ -7,35 +12,199 @@ import java.net.*;
  * @author www.codejava.net
  */
 
-public class ServerThread {
-    private Socket socket;
+public class ServerThread extends Thread{
+    //??
+    protected ServerSocket serverSocket = null;
+    protected BufferedReader in = null;
 
-    public ServerThread(Socket socket) {
-        this.socket = socket;
+    protected String thread_name = null;
+    protected DatagramSocket socket = null;
+    protected Random rand = null;
+    protected static int psecret;
+
+    public static final int PORT_NUM = 12235;
+    public static final int HEADERSPACE = 12;
+
+    public ServerThread() throws IOException {
+        this("ServerThread");
+    }
+
+    public ServerThread(String name) throws IOException {
+        this.thread_name = name;
+        this.socket = new DatagramSocket(PORT_NUM);
+        this.rand = new Random();
+        this.psecret = 0;
+
+        //??
+        this.serverSocket = new ServerSocket(PORT_NUM);
     }
 
     public void run() {
-        try {
-            InputStream input = socket.getInputStream();
+        try (
+            Socket clientSocket = serverSocket.accept();
+
+            InputStream input = clientSocket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
-            OutputStream output = socket.getOutputStream();
+            OutputStream output = clientSocket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
 
+        ) {
+            // Stage A
+            // receiving packet
+            System.out.print("Stage A running...");
+            byte[] receive_buffer = new byte[HEADERSPACE + "hello world\0".length()];
+            DatagramPacket packet = new DatagramPacket(receive_buffer, receive_buffer.length);
+            socket.receive(packet);
+            String result = new String(packet.getData(), HEADERSPACE, "hello world\0".length());
+            // verify received message "hello world\0"
+            if (!result.equals("hello world\0")) {
+                System.out.println("    Wrong message received in Stage A");
+            } else {
+                System.out.println("    Message Received: " + result);
+            }
+            // prepare sending packet
+            ByteBuffer b = ByteBuffer.allocate(16);
+            int num = (int) (Math.random() * (10 - 5 + 1) + 5);     // [5, 10)
+            int len = (int) (Math.random() * (500 - 5 + 1) + 5);    // [5, 500)
+            int udp_port = (int) (Math.random() * (65535 - 49152 + 1) + 49152);   // Ephemeral port range [49152, 65535)
+            int secretA = (int) (Math.random() * (500 - 5 + 1) + 5);// [5, 500)
+            System.out.println("    num: " + num);
+            System.out.println("    len: " + len);
+            System.out.println("    udp_port: " + udp_port);
+            System.out.println("    secretA: " + secretA);
+            b.putInt(num);          // num
+            b.putInt(len);          // len
+            b.putInt(udp_port);     // udp_port
+            b.putInt(secretA);      // secretA
+            byte[] payload_a2 = b.array();
+            byte[] send_buffer = bufferCreate(payload_a2, secretA, (short) 2);
+            // send packet
+            InetAddress client_addr = packet.getAddress();
+            int client_port = packet.getPort();
+            packet = new DatagramPacket(send_buffer, send_buffer.length, client_addr, client_port);
+            socket.send(packet);
+            System.out.println("Stage A finished...\n\n");
 
+
+            // Stage B
+            System.out.print("Stage B running...");
+            int count = 0;
+            int payload_b1_len = (len % 4 == 0) ? len : (len / 4 * 4 + 4);  // 4-byte alignment
+
+            //  b1
+            Boolean ack = false;
+            while (count < num) {
+                receive_buffer = new byte[HEADERSPACE + 4 + payload_b1_len];  // 4: packet_id length
+                this.socket = new DatagramSocket(udp_port);
+                packet = new DatagramPacket(receive_buffer, receive_buffer.length);
+                socket.receive(packet);
+                int[] payload_b1 = receiveHandler(receive_buffer, payload_b1_len / 4 + 1);
+                // verification
+                System.out.println("    packet_id: " +  payload_b1[0]);
+                for (int i = 1; i < payload_b1.length; i++) {
+                    if (payload_b1[i] != 0) {
+                        System.out.println("    receive non-zero at index: " + count);
+                    }
+                }
+                // ack
+                if (!ack) { // at lease one !ack
+                    ack = true;
+                    continue;
+                }
+                if (rand.nextBoolean()) {
+                    ByteBuffer acked_packet_id = ByteBuffer.allocate(4);
+                    acked_packet_id.putInt(payload_b1[0]);
+                    byte[] ack_payload = b.array();
+                    send_buffer = bufferCreate(ack_payload, psecret, (short) 1);
+                    client_addr = packet.getAddress();
+                    client_port = packet.getPort();
+                    packet = new DatagramPacket(send_buffer, send_buffer.length, client_addr, client_port);
+                    socket.send(packet);
+                    count++;
+                }
+            }
+            // b2
+            ByteBuffer byteBuffer = ByteBuffer.allocate(8);
+            int tcp_port = (int) (Math.random() * (65535 - 49152 + 1) + 49152); // Ephemeral port range [49152, 65535)
+            int secretB = (int) (Math.random() * (500 - 5 + 1) + 5);            // [5, 500);
+            byteBuffer.putInt(tcp_port);
+            byteBuffer.putInt(secretB);
+            byte[] payload_b2 = b.array();
+            send_buffer = bufferCreate(payload_b2, psecret, (short) 1);
+            client_addr = packet.getAddress();
+            client_port = packet.getPort();
+            packet = new DatagramPacket(send_buffer, send_buffer.length, client_addr, client_port);
+            socket.send(packet);
+            System.out.println("Stage B finished...\n\n");
+
+
+            System.out.print("Stage C running...");
+
+            System.out.println("Stage C finished...\n\n");
+
+            /*
+            String stageA = null;
+            if (reader == null) {
+                System.out.println("fail to build input reader");
+            } else {
+                if ((stageA = reader.readLine()) == null) {
+                    reader.close();
+                    System.out.println("receive no message in stage A");
+                }
+            }
+            read_buf = stageA.getBytes();
+            System.out.println("Stage A message: " + Arrays.toString(read_buf));
+*/
+            /*
             String text;
-
             do {
                 text = reader.readLine();
                 String reverseText = new StringBuilder(text).reverse().toString();
                 writer.println("Server: " + reverseText);
 
             } while (!text.equals("bye"));
-
-            socket.close();
+            */
+            serverSocket.close();
         } catch (IOException ex) {
             System.out.println("Server exception: " + ex.getMessage());
             ex.printStackTrace();
         }
+    }
+
+
+    private static int[] receiveHandler(byte[] receiveBuffer, int len) {
+        int[] res = new int[len];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(receiveBuffer);
+        headerHandler(byteBuffer);
+        for (int i = 0; i < len; i++) {
+            res[i] = byteBuffer.getInt();
+        }
+
+        return res;
+    }
+
+    private static void headerHandler(ByteBuffer byteBuffer) {
+        byteBuffer.getInt(); // payload_len
+        psecret = byteBuffer.getInt(); // psecret
+        byteBuffer.getShort(); // step
+        byteBuffer.getShort(); // last 3 digits of student number
+    }
+
+    private static byte[] bufferCreate(byte[] buffer, int pSecret, short step) {
+        // Student number : 1836832
+        short studentNumber = 832;
+        // the alignment of the payload is 4, add padding bytes.
+        int bufferSpace = buffer.length;
+        while (bufferSpace % 4 != 0) {
+            bufferSpace++;
+        }
+        ByteBuffer byteBuffer = ByteBuffer.allocate(bufferSpace + HEADERSPACE);
+        byteBuffer.putInt(buffer.length);
+        byteBuffer.putInt(pSecret);
+        byteBuffer.putShort(step);
+        byteBuffer.putShort(studentNumber);
+        byteBuffer.put(buffer);
+        return byteBuffer.array();
     }
 }

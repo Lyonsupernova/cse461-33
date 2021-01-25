c689b742ce860f6d78e145539e19c73af6a2d5b9
Lily Zhao <danyaz3@uw.edu>
2021-01-24 23:38:19 -0800

    4

--- Project 1/ServerThread.java
+++ Project 1/ServerThread.java
@@ -201,8 +201,9 @@ public class ServerThread extends Thread{
 
             // Step c1
             this.serverSocket = new ServerSocket(tcp_port);
+            System.out.println("create server socket succeeded.");
             Socket tcp_socket = serverSocket.accept();
-            System.out.println("Accep tcp socket succeeded.");
+            System.out.println("Accept tcp socket succeeded.");
             int len2 = stepc1(tcp_socket);
             System.out.println("Stage C finished...\n\n");
 

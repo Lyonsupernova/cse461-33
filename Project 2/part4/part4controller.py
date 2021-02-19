2fbca486f63770df3726e2019732b7675e077f07
“lyons” <lyons124@uw.edu>
2021-02-19 23:28:34 +0800

    fix part3 and finished most of part4... Need run part4 not sure...

--- Project 2/part4/part4controller.py
+++ Project 2/part4/part4controller.py
@@ -1,14 +1,19 @@
-# Part 3 of UWCSE's Project 3
+# Part 4 of UWCSE's Project 3
 #
 # based on Lab Final from UCSC's Networking Class
 # which is based on of_tutorial by James McCauley
 
+from struct import pack
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.addresses import IPAddr, IPAddr6, EthAddr
+from pox.lib.packet import arp
+from pox.lib.packet import ethernet
 
 log = core.getLogger()
 
+# the set stores all the ips learned
+ip_learn_set = set()
 #statically allocate a routing table for hosts
 #MACs used in only in part 4
 IPS = {
@@ -19,7 +24,7 @@ IPS = {
   "hnotrust" : ("172.16.10.100", '00:00:00:00:00:05'),
 }
 
-class Part3Controller (object):
+class Part4Controller (object):
   """
   A Connection object for that switch is passed to the __init__ function.
   """
@@ -58,6 +63,8 @@ class Part3Controller (object):
   def core_set_up_arp(self, name, port):
     msg = of.ofp_flow_mod()
     msg.priority = 33001
+    # ARP requests, learning the port from the hosts
+
     msg.match.dl_type = 0x0806
     msg.match.nw_dst = IPAddr([name][0])
     msg.actions.append(of.ofp_action_output(port = port))
@@ -138,6 +145,8 @@ class Part3Controller (object):
     msg.actions.append(action)
     self.connection.send(msg)
 
+
+  
   def _handle_PacketIn (self, event):
     """
     Packets not handled by the router rules will be
@@ -149,7 +158,59 @@ class Part3Controller (object):
       log.warning("Ignoring incomplete packet")
       return
 
+    cores21_mac_address = EthAddr("de:ed:be:ef:ca:fe")
+ 
     packet_in = event.ofp # The actual ofp_packet_in message.
+    port_num = packet_in.in_port # the port number of sending the packet
+    mac_addr_host = EthAddr(packet.src) # the mac address of host
+    
+    # packet receives ARP request needs to respond an ARP message
+    if packet.type == packet.ARP_TYPE:
+       if packet.payload.opcode == arp.REQUEST:
+        arp_packet = packet.find('arp')
+        # get the ip source
+        ip_addr_host = arp_packet.protosrc
+        # make an arp reply
+        arp_reply = arp()
+        # src would be the mac address of cores21
+        arp_reply.hwsrc = cores21_mac_address
+        arp_reply.hwdst = packet.src
+        arp_reply.opcode = arp.REPLY
+        # ip of requested mac-associated machine
+        arp_reply.protosrc = arp_packet.protodst
+        arp_reply.protodst = packet.payload.protosrc
+
+        # ethernet wrap in
+        ether = ethernet()
+        ether.type = ethernet.ARP_TYPE
+        ether.dst = packet.src
+        ether.src = cores21_mac_address
+        
+        # resend the packet
+        ether.set_payload(arp_reply)
+        self.resend_packet(self, arp_reply, port_num)
+
+        if ip_addr_host not in ip_learn_set :
+          # cores21 install a rule on the address and port
+          msg = of.ofp_flow_mod()
+          msg.priority = 33001
+          msg.match.nw_dst = IPAddr(ip_addr_host)
+          msg.match.dl_type = 0x0806
+          # arp translate from ip to mac
+          msg.actions.append(of.ofp_action_output(port = port_num))
+          msg.actions.append(of.ofp_action_dl_addr.set_dst(mac_addr_host))
+          self.connection.send(msg)
+          
+          # ip address
+          msg = of.ofp_flow_mod()
+          msg.priority = 33001
+          msg.match.nw_dst = IPAddr(ip_addr_host)
+          msg.match.dl_type = 0x0800
+          self.connection.send(msg)
+
+
+          ip_learn_set.add(ip_addr_host)
+   
     print ("Unhandled packet from " + str(self.connection.dpid) + ":" + packet.dump())
 
 def launch ():
@@ -158,6 +219,8 @@ def launch ():
   """
   def start_switch (event):
     log.debug("Controlling %s" % (event.connection,))
-    Part3Controller(event.connection)
+    Part4Controller(event.connection)
   
   core.openflow.addListenerByName("ConnectionUp", start_switch)
+
+
